24670821c7d19708b74c3534fecd2bed45a3ab30
Waldemar Tomme <waldetomme@gmail.com>
2022-10-10 16:03:09 +0200

    Enable intellisense

--- source.c
+++ source.c
@@ -1,4 +1,8 @@
+#ifdef QMK_KEYBOARD_H
 #include QMK_KEYBOARD_H
+#else
+#include "quantum/quantum.h" // only for intellisense
+#endif
 
 void keyboard_post_init_user(void) {
     // Turn off on-board leds
@@ -8,12 +12,12 @@ void keyboard_post_init_user(void) {
     writePinHigh(D5);
 }
 
-#ifdef CONSOLE_ENABLE
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+#ifdef CONSOLE_ENABLE
     // If console is enabled, it will print the matrix position and status of each key pressed
     if (debug_enable) {
         uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
     }
+#endif
     return true;
 }
-#endif
