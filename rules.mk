dc6e4e71906b62cc325cd650415d643f9df498c1
Waldemar Tomme <waldetomme@gmail.com>
2022-10-10 13:37:27 +0200

    Disable console and describe ca. rom usage

--- rules.mk
+++ rules.mk
@@ -2,5 +2,6 @@ SRC += source.c
 
 BOOTMAGIC_ENABLE = yes
 MOUSEKEY_ENABLE = no
-CONSOLE_ENABLE = yes
-RGBLIGHT_ENABLE = yes
+# CONSOLE_ENABLE = yes # ~18%
+RGBLIGHT_ENABLE = yes # ~13%
+# AUDIO_ENABLE = yes # ~19%
--- source.c
+++ source.c
@@ -14,12 +14,12 @@ void keyboard_post_init_user(void) {
     writePinHigh(D5);
 }
 
+#ifdef CONSOLE_ENABLE
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // If console is enabled, it will print the matrix position and status of each key pressed
-#ifdef CONSOLE_ENABLE
     if (debug_enable) {
         uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
     }
-#endif
     return true;
 }
+#endif
