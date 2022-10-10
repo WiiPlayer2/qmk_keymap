37446fe5f144bef1cbd9ccba8e385a7632d182be
Waldemar Tomme <waldetomme@gmail.com>
2022-10-10 15:16:09 +0200

    Add audio feature

--- source.c
+++ source.c
@@ -1,12 +1,6 @@
 #include QMK_KEYBOARD_H
 
 void keyboard_post_init_user(void) {
-    // Customise these values to desired behaviour
-    // debug_enable=true;
-    // debug_matrix=true;
-    // debug_keyboard=true;
-    //debug_mouse=true;
-
     // Turn off on-board leds
     setPinOutput(B0);
     writePinHigh(B0);
