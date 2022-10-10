7c3e9edf07dd7487ad91fc604e0cf954b5e3c57f
Waldemar Tomme <waldetomme@gmail.com>
2022-10-10 15:33:55 +0200

    Reduce firmware size a bit

--- rules.mk
+++ rules.mk
@@ -5,4 +5,5 @@ MOUSEKEY_ENABLE = no
 # CONSOLE_ENABLE = yes # ~18%
 RGBLIGHT_ENABLE = yes # ~13%
 AUDIO_ENABLE = yes # ~19%
+MUSIC_ENABLE = no
 # VELOCIKEY_ENABLE = yes # ~1%, currently not working with split
